df9a06b2eba1866ada47246d60925d15c80a60ff
Farhan-2007 <shaikhmf9322@gmail.com>
2026-06-30 00:42:22 +0530

    Updated blog post routes

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,7 @@ from app.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 import secrets
 import os
-
+from PIL import Image
 posts = [
     {
         "author" : "Farhan Shaikh",
@@ -71,7 +71,10 @@ def save_picture(form_picture):
      _, f_ext = os.path.splitext(form_picture.filename)
      picture_fn = random_hex + f_ext
      picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
-     form_picture.save(picture_path)
+     output_size = (125, 125)
+     i = Image.open(form_picture)
+     i.thumbnail(output_size)
+     i.save(picture_path)
      return picture_fn
     
 
